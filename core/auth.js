69b222c918c852566f0c026fbef74c5dac46b951
fuwenlong <long@sadais.com>
2020-09-25 19:15:19 +0800

    feat: 刷新token完善

---
+++ core/auth.js
@@ -0,0 +1,147 @@
+
+
+
+
+const USER_INFO = 'USER_INFO' // 用户个人信息
+const USER_ACCOUNT = 'USER_ACCOUNT' // 用户账号
+const LOGIN_INFO = 'LOGIN_INFO' // 登录信息
+const TOKEN_ID = 'TOKEN_ID' // TOKEN令牌ID
+const REFRESH_TOKEN_ID = 'REFRESH_TOKEN_ID' // 刷新token时使用的token
+
+/**
+ * 保存用户信息到本地存储中
+ * @param {Object} loginInfo 用户信息
+ */
+export const saveLoginInfo = loginInfo => {
+  const result = uni.setStorageSync(LOGIN_INFO, loginInfo)
+  return result
+}
+
+/**
+ * 获取登录信息
+ * @param {Object} loginInfo 用户信息
+ */
+export const getLoginInfo = () => {
+  const loginInfo = uni.getStorageSync(LOGIN_INFO)
+  return loginInfo
+}
+
+/**
+ * 保存用户信息到本地存储中
+ * @param {Object} user 用户信息
+ */
+export const saveUserInfo = user => {
+  const result = uni.setStorageSync(USER_INFO, user)
+  return result
+}
+
+/**
+ * 移除用户信息
+ */
+export const removeUserInfo = () => {
+  return uni.removeStorageSync(USER_INFO)
+}
+
+/**
+ * 获取用户账号
+ */
+export const getUserAccount = () => {
+  const userAccount = uni.getStorageSync(USER_ACCOUNT)
+  return userAccount
+}
+
+/**
+ * 移除令牌ID
+ */
+export const removeUserAccount = () => {
+  return uni.removeStorageSync(USER_ACCOUNT)
+}
+
+/**
+ * 从本地存储中获取用户ID
+ */
+export const getUserid = () => {
+  const user = uni.getStorageSync(USER_INFO) || {}
+  return user.userid || user.userId || ''
+}
+
+/**
+ * 保存令牌ID到本地存储中
+ * @param {String} tokenid 令牌
+ */
+export const saveTokenid = tokenid => {
+  uni.setStorage({
+    key: TOKEN_ID,
+    data: tokenid
+  })
+}
+
+/**
+ * 从本地存储中获取令牌ID
+ */
+export const getTokenid = () => {
+  return uni.getStorageSync(TOKEN_ID) || ''
+}
+
+/**
+ * 移除令牌ID
+ */
+export const removeTokenid = () => {
+  return uni.removeStorageSync(TOKEN_ID)
+}
+
+/**
+ * 保存刷新token的令牌ID到本地存储中
+ * @param {String} refreshToken 令牌
+ */
+export const saveRefreshTokenid = refreshToken => {
+  uni.setStorageSync(REFRESH_TOKEN_ID, refreshToken)
+}
+
+/**
+ * 从本地存储中获取令牌ID
+ */
+export const getRefreshTokenid = () => {
+  return uni.getStorageSync(REFRESH_TOKEN_ID) || ''
+}
+
+/**
+ * 移除令牌ID
+ */
+export const removeRefreshTokenid = () => {
+  return uni.removeStorageSync(REFRESH_TOKEN_ID)
+}
+
+/**
+ * 重定向到登录页
+ */
+export const redirectToLogin = url => {
+  // 重定向登录页面
+  redirectTo(url || consts.LOGIN_PATH)
+}
+
+/**
+ * 关闭所有页面打开登录页面
+ */
+export const reLaunchToLogin = url => {
+  const hash = window && window.location && window.location.hash
+  // 如果已在登录页就不再进入
+  if (!hash || hash.indexOf(consts.LOGIN_PATH) < 0) {
+    reLaunch(url || consts.LOGIN_PATH)
+  }
+}
+
+/**
+ * 用户注销
+ * @param {String} url 注销后重定向登录的地址
+ */
+export const logout = url => {
+  console.log('用户注销，清除相关用户信息')
+  removeTokenid()
+  removeRefreshTokenid()
+  removeUserInfo()
+  removeUserAccount()
+  reLaunchToLogin(url || consts.LOGIN_PATH)
+}
+
+
--- core/request.js
+++ core/request.js
@@ -1,8 +1,145 @@
 import Request from 'luch-request'
 import { getConsts } from '../consts'
+import { getUserid, saveTokenid, getTokenid, saveRefreshTokenid, getRefreshTokenid } from './auth'
+const RETRY_TOKEN_TIME = 10 // 重新获取token次数
 
-const requestInstance = new Request()
+const options = {
+  baseURL: getConsts('API_BASE_URL'),
+  header: {
+    'sadais-agent': _getSadaisAgent()
+  },
+  custom: {
+    currentTokenRetry: RETRY_TOKEN_TIME // 当前token刷新次数
+  }
+}
+const requestInstance = new Request(options)
 
-requestInstance.config.baseURL = getConsts('API_BASE_URL')
+/**
+ * 获取SadaisAgent
+ */
+function _getSadaisAgent() {
+  if (getConsts('SADAIS_AGENT')) {
+    const appName = getConsts('NAME')
+    const appVersion = getConsts('VERSION')
+    const sysInfo = uni.getSystemInfoSync()
+    const system = getConsts('SYSTEM')
+    const channel = ''
+    const devInfo = sysInfo ? '(' + sysInfo.brand + ';' + sysInfo.system + ')' : ''
+    const station = getConsts('STATION')
+
+    // 应用英文名称（全大写）/当前版本/系统（全大写）/渠道信息/系统信息/网络信息/运营商，
+    const sadaisAgent = [appName, appVersion, system, channel, devInfo, '', station].join('/')
+    return sadaisAgent
+  }
+  return ''
+}
+
+/**
+ * 获取token对象
+ */
+function _getToken(requestUrl) {
+  // 首先判断是否是刷新token的接口，如果是则取刷新token的专用refreshToken
+  if (getConsts('NEED_REFRESH_TOKEN_LIST').includes(requestUrl)) {
+    const refreshTokenid = getRefreshTokenid()
+    const token = {
+      [getConsts('TOKEN_KEY')]: refreshTokenid
+    }
+    return token
+  }
+  // 数据签名
+  const tokenid = getTokenid()
+  if (!getConsts('TOKEN_WHITE_LIST').includes[requestUrl] && tokenid) {
+    const token = {
+      [getConsts('TOKEN_KEY')]: tokenid
+    }
+    return token
+  }
+  return {}
+}
+
+const refreshTokenIfNeed = async response => {
+  if (
+    response &&
+    response.data &&
+    response.data.head &&
+    getConsts('TOKEN_INVALID_CODE').includes(response.data.head.ret)
+  ) {
+    const userId = getUserid()
+    if (!userId || !response.config.custom.currentTokenRetry) {
+      reLaunchToLogin()
+      return response
+    }
+    response.config.custom.currentTokenRetry--
+
+    const data = await apiGetToken()
+    if (data && data.head && data.head.ret === 0) {
+      response.config.custom.currentTokenRetry = RETRY_TOKEN_TIME // 重置token重试次数
+      // 刷新token成功 保存token
+      const accessToken = data.data && data.data.accesstoken
+      const refreshtoken = data.data && data.data.refreshtoken
+
+      saveTokenid(accessToken)
+      refreshtoken && saveRefreshTokenid(refreshtoken)
+      // 重新发起请求
+      const newResponse = await requestInstance.request(response.config)
+      return newResponse
+    }
+  }
+  return response
+}
+
+const apiGetToken = async () => {
+  const { data } = await requestInstance.get(getConsts('TOKEN_API'))
+  return data
+}
+
+requestInstance.interceptors.request.use(
+  config => {
+    /* 请求之前拦截器。可以使用async await 做异步操作 */
+    config.header = {
+      ...config.header
+    }
+
+    const token = _getToken(config.url)
+    if (token) {
+      config.header = {
+        ...config.header,
+        ...token
+      }
+    }
+
+    // 如果post请求 没有设置content-type 则默认application/json
+    if (!config.header['content-type'] && config.method.toUpperCase() === 'POST') {
+      config.header['content-type'] = 'application/json;charset=UTF-8'
+    }
+
+    return config
+  },
+  config => {
+    return Promise.reject(config)
+  }
+)
+
+requestInstance.interceptors.response.use(
+  async response => {
+    const statusCode = response.statusCode
+    /* 请求之后拦截器。可以使用async await 做异步操作  */
+    if (statusCode !== 200) {
+      // 服务端返回的状态码不等于200，则reject()
+      return Promise.reject(response)
+    }
+
+    if (getConsts('TOKEN_EXCEPTION_PROCESS')) {
+      response = await refreshTokenIfNeed(response)
+    }
+    return response
+  },
+  response => {
+    // 请求异常处理
+    console.log(response)
+    return Promise.reject(response)
+  }
+)
 
 export default requestInstance
+
